8029a86cfe74591051d75570ef03458cf4522e5e
Leandro Lima <llimas@outlook.com>
2022-02-24 22:52:09 -0300

    Making this work

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/LeandroLS/go-ascii-image
 
 go 1.17
 
-require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/jpeg"
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
@@ -13,17 +14,13 @@ import (
 )
 
 func getChar(grayScale int) string {
-	// densityCharsArr := []string{"$", "@", "B", "%", "8", "&", "W", "M", "#", "*", "o", "a", "h", "k", "b", "d", "p", "q", "w", "m", "Z", "O", "0", "Q", "L", "C", "J", "U", "Y", "X", "z", "c", "v", "u", "n", "x", "r", "j", "f", "t", "/", "\\", "|", "(", ")", "1", "{", "}", "[", "]", "?", "-", "_", "+", "~", "<", ">", "i", "!", "l", "I", ";", ":", ",", ",", "^", "`", "'", "."}
-	// densityCharsArr := []string{"#", "W", "o", "-", " "}
-	densityCharsArr := []string{" ", "-", "o", "W", "#"}
+	densityCharsArr := []string{"  ", "--", "oo", "WW", "##"}
 
 	interval := float64(len(densityCharsArr)) / float64(256)
 	return densityCharsArr[int(math.Floor(float64(grayScale)*interval))]
 }
 
 func main() {
-	oneCharWidth := 8
-	oneCharHeight := 18
 	imgfile, err := os.Open("./lion.jpg")
 
 	if err != nil {
@@ -32,34 +29,20 @@ func main() {
 	}
 
 	defer imgfile.Close()
-
-	// get image height and width with image/jpeg
-	// change accordinly if file is png or gif
-
-	imgCfg, _, err := image.DecodeConfig(imgfile)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	width := imgCfg.Width
-	height := imgCfg.Height
-
-	fmt.Println("Width : ", width)
-	fmt.Println("Height : ", height)
-
-	// we need to reset the io.Reader again for image.Decode() function below to work
-	// otherwise we will  - panic: runtime error: invalid memory address or nil pointer dereference
-	// there is no build in rewind for io.Reader, use Seek(0,0)
 	imgfile.Seek(0, 0)
 
-	// get the image
 	img, _, _ := image.Decode(imgfile)
-	imgResized := resize.Resize(uint(width), uint(height*(oneCharWidth/oneCharHeight)), img, resize.Lanczos3)
+	imgResized := resize.Resize(50, 0, img, resize.Lanczos3)
+
+	out, err := os.Create("test_resized.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	out.Seek(0, 0)
+	// write new image to file
+	jpeg.Encode(out, imgResized, nil)
 	f, _ := os.Create("data.txt")
-	fmt.Println("Width : ", width)
-	fmt.Println("Height : ", height)
 
 	if err != nil {
 		log.Fatal(err)
@@ -67,10 +50,37 @@ func main() {
 
 	defer f.Close()
 
+	imgfile2, err := os.Open("./test_resized.jpg")
+
+	if err != nil {
+		fmt.Println("img.jpg file not found!")
+		os.Exit(1)
+	}
+
+	defer imgfile2.Close()
+
+	imgCfg, _, err := image.DecodeConfig(imgfile2)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	imgfile2.Seek(0, 0)
+
+	width := imgCfg.Width
+	height := imgCfg.Height
+
+	fmt.Println("Width : ", width)
+	fmt.Println("Height : ", height)
+
+	img2, _, _ := image.Decode(imgfile2)
+
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			r, g, b, _ := imgResized.At(x, y).RGBA()
+			r, g, b, _ := img2.At(x, y).RGBA()
+			fmt.Println(r, g, b)
 			avg := uint8((r + g + b) / 3)
+			fmt.Println(avg)
 			_, err2 := f.WriteString(getChar(int(avg)))
 			if err2 != nil {
 				log.Fatal(err2)
