f8fb36b3841e4509e9c01e7f7b06c1b695b41518
Leandro Lima <llimas@outlook.com>
2022-02-26 19:53:45 -0300

    Deleting unecessary code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"image"
-	"image/jpeg"
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
@@ -13,8 +12,6 @@ import (
 )
 
 func getChar(grayScale int) string {
-	// densityCharsArr := []string{"  ", "--", "oo", "WW", "##"}
-	// densityCharsArr := []string{"==","==","==","==","==","==","==","--","--","--","--","--","--","--","--","::","::","::","::","::","::","::","::","::","..","..","..","..","..","..","..",".."}
 	densityCharsArr := []string{"@@", "@@", "@@", "@@", "@@", "@@", "@@", "@@", "%%", "%%", "%%", "%%", "%%", "%%", "%%", "%%", "##", "##", "##", "##", "##", "##", "##", "##", "##", "**", "**", "**", "**", "**", "**", "**", "**", "++", "++", "++", "++", "++", "++", "++", "++", "++", "==", "==", "==", "==", "==", "==", "==", "=="}
 	interval := float64(len(densityCharsArr)) / float64(256)
 	return densityCharsArr[int(math.Floor(float64(grayScale)*interval))]
@@ -26,7 +23,7 @@ func openImgFile(imgFileName string) image.Image {
 		log.Fatal(err)
 	}
 	defer imgOpened.Close()
-	imgOpened.Seek(0, 0)
+
 	img, _, err := image.Decode(imgOpened)
 	if err != nil {
 		log.Fatal(err)
@@ -34,27 +31,10 @@ func openImgFile(imgFileName string) image.Image {
 	return img
 }
 
-func resizeImg(image image.Image) *os.File {
-	userImgResized := resize.Resize(100, 0, image, resize.Lanczos3)
-	newUserImgResized, err := os.Create("resized-img.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer newUserImgResized.Close()
-	newUserImgResized.Seek(0, 0)
-
-	jpeg.Encode(newUserImgResized, userImgResized, nil)
-	imgResized, err := os.Open("resized-img.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return imgResized
-
-}
-
 func main() {
 	userImg := openImgFile("./cat.jpg")
-	imgResized := resizeImg(userImg)
+
+	userImgResized := resize.Resize(100, 0, userImg, resize.Lanczos3)
 
 	f, err := os.Create("ascii.txt")
 
@@ -64,11 +44,10 @@ func main() {
 
 	defer f.Close()
 
-	img, _, _ := image.Decode(imgResized)
-	grayImg := image.NewGray(img.Bounds())
-	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
-		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
-			grayImg.Set(x, y, img.At(x, y))
+	grayImg := image.NewGray(userImgResized.Bounds())
+	for y := userImgResized.Bounds().Min.Y; y < userImgResized.Bounds().Max.Y; y++ {
+		for x := userImgResized.Bounds().Min.X; x < userImgResized.Bounds().Max.X; x++ {
+			grayImg.Set(x, y, userImgResized.At(x, y))
 			r, g, b, _ := grayImg.At(x, y).RGBA()
 			avg := uint8((r + g + b) / 3)
 			_, err := f.WriteString(getChar(int(avg)))
